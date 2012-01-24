534b6f8944df23e89bdf7bde25183dee8d162e59
Rohit Arolkar <rohit.arolkar@livialegal.com>
2012-01-24 20:28:39 +0530

    moved JS to js_file and works for different calenders

--- public/javascripts/calendar/calendar_main.js
+++ public/javascripts/calendar/calendar_main.js
@@ -1,7 +1,45 @@
 $(document).ready(function() {
-  alert('hi!');
-
-  $('#calendar_check_box').click(function(){
-    alert('hi!');
+  buildCalendar();
+  $('.calendar_check_boxes_set').each(function(index,Element){
+    showCalendar($(this).find("input[type=checkbox]").attr('id'));
   });
 });
+
+function get_source(id){
+  var cal_appointments = {
+    url: "/calendars/" + id.toString() +"/appointments"
+  };
+  return cal_appointments;
+}
+
+function toggleSource(checkbox) {
+  if(checkbox.checked == false ){
+    hideCalendar(checkbox.id);
+  }
+  else{
+    showCalendar(checkbox.id);
+  }
+}
+
+function hideCalendar(id) {
+  $('#calendar').fullCalendar('removeEventSource',get_source(id));
+}
+
+function showCalendar(id) {
+  $('#calendar').fullCalendar('addEventSource',get_source(id));
+}
+
+function buildCalendar() {
+  $('#calendar').fullCalendar({
+    theme: true,
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    editable: false,
+    weekMode: 'liquid',
+    eventSources: []
+  });
+}
+
