a04bfb94a19bea9fd8ee7d4eb8c4babfe380a4c0
Rahul P. Chaudhari <rahul100885@gmail.com>
2012-01-30 20:54:35 +0530

    Added crud operation to calendar and added links

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,8 +2,70 @@ class CalendarsController < ApplicationController
   
   def index
     @calendars = Calendar.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @calendars }
+    end
   end
 
   def show
+    @calendar = Calendar.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @calendar }
+    end
+  end
+
+  def new
+    @calendar = Calendar.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @calendar }
+    end
+  end
+
+  def edit
+    @calendar = Calendar.find(params[:id])
+  end
+
+  def create
+    @calendar = Calendar.new(params[:calendar])
+
+    respond_to do |format|
+      if @calendar.save
+        format.html { redirect_to(@calendar, :notice => 'Calendar was successfully created.') }
+        format.xml  { render :xml => @calendar, :status => :created, :location => @calendar }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @calendar.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @calendar = Calendar.find(params[:id])
+
+    respond_to do |format|
+      if @calendar.update_attributes(params[:calendar])
+        format.html { redirect_to(@calendar, :notice => 'Calendar was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @calendar.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @calendar = Calendar.find(params[:id])
+    @calendar.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(calendars_url) }
+      format.xml  { head :ok }
+    end
   end
 end
