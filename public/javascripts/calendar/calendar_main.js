098459caabe0a5ddb6cc79cfbf7a6ea15b91b282
Rahul P. Chaudhari <rahul100885@gmail.com>
2012-02-10 19:16:28 +0530

    Calendar added external events temporary

--- public/javascripts/calendar/calendar_main.js
+++ public/javascripts/calendar/calendar_main.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   buildCalendar();
+
   $('.calendar_check_boxes_set').each(function(index,Element){
     showCalendar($(this).find("input[type=checkbox]").attr('id'));
   });
+
+  bindEventToCalendar();
 });
 
 function get_source(id){
@@ -30,7 +33,7 @@ function showCalendar(id) {
 }
 
 function buildCalendar() {
-  $('#calendar').fullCalendar({
+  calObj = $('#calendar').fullCalendar({
     theme: true,
     header: {
       left: 'prev,next today',
@@ -43,3 +46,35 @@ function buildCalendar() {
   });
 }
 
+function bindEventToCalendar(){
+  $('#calendar_prev').click( function() {
+    calObj.fullCalendar('prev');
+  });
+
+  $('#calendar_next').click( function() {
+    calObj.fullCalendar('next');
+  });
+
+  $('#calendar_today').click( function() {
+    calObj.fullCalendar('today');
+  });
+
+  $('#calendar_monthview').click( function() {
+    calObj.fullCalendar('changeView', 'month');
+  });
+
+  $('#calendar_weekview').click( function() {
+    calObj.fullCalendar('changeView', 'agendaWeek');
+  });
+
+  $('#calendar_dayview').click( function() {
+    calObj.fullCalendar('changeView', 'agendaDay');
+  });
+
+  $('#calendar_title').text("ok");
+
+  $('.calendar_title').click( function(){
+    $('#calendar_title').text(calObj.fullCalendar('getView').title);
+  });
+
+}
